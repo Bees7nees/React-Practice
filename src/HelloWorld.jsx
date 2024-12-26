0a528f964e18651f7ab5d7b111d09dd0f792560a
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2024-12-26 17:32:37 +0000

    Starting from scratch

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,11 @@
+#! Following Develhope's "Rendering a component" example
+
+export HelloWorld = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default HelloWorld
