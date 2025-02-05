c4c6ca4f1b8c2e7215a4927187506dc0c6c8e4c3
Bees7nees <150014677+Bees7nees@users.noreply.github.com>
2025-02-05 17:51:23 +0000

    New react + vite project

---
+++ React Basics freeCodeCamp/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <section className="navbar">
+      <a href="#">
+        <h1>Home</h1>
+      </a>
+      <a href="#">
+        <h1>About</h1>
+      </a>
+      <a href="#">
+        <h1>Contact</h1>
+      </a>
+    </section>
+  );
+}
+
+export default Navbar
